9982f67767890a99b8dd2eb2613338779bef622a
Jeremy Wall <jeremy@marzhillstudios.com>
2008-12-20 22:19:02 -0600

    expanded the record definitions for RSS Really Simple Syndication

--- src/rss.erl
+++ src/rss.erl
@@ -9,21 +9,47 @@
 
 -export([process_rss/1, parse_rss_channels/1]).
 
-%% @type channel() = #channel{uri=Uri, title=Title, desc=Desc, items=Items}
-%%  Items = [ rssitem() ]
-%%  Uri = string()
-%%  Title = string()
-%%  Desc = string().
+%% @type channel() = #channel{
+%%              uri=string(), title=string(), desc=string(), 
+%%              items=Items,
+%%              language=string(), copyright=string(), 
+%%              managing_editor=string(), web_master=string(), 
+%%              pub_date=string(), last_build_date=string(), category=rss_cat(),
+%%              generator=string(), 
+%%              docs=string(), cloud=rss_cloud(), ttl=number(),
+%%              image=rss_image(), rating=string(), text_input=rss_text_input(), 
+%%              skip_hours=rss_number_series(), skip_days=rss_number_series()}
+%%  Items = [ rssitem() ].
 
-%% @type rssitem() = #rssitem{uri=Uri, title=Title, desc=Desc, content=Content, dt=Dt}
-%%  Uri = string()
-%%  Title = string()
-%%  Desc = string()
-%%  Content = string()
-%%  Dt = string().
+%% @type rssitem() = #rssitem{
+%%              uri=string(), title=string(), desc=string(), author=string(),
+%%              pub_date=string(), enclosure=Enclosure, comments=string(),
+%%              category=Category, guid=Guid, source=Source}
+%%  Enclosure = rss_enclosure()
+%%  Category = rss_cat()
+%%  Guid = rss_guid().
 
--record(channel, {uri, title, desc, items}).
--record(rssitem, {uri, title, desc, content, dt}).
+%% @type rss_enclosure() = #rssenclosure{url=string(), length=string(), type=string()}.
+
+%% @type rss_cat() = #rsscategory{domain=string(), value=string()}.
+
+%% @type rss_guid() = #rssguid{isPermaLink=bool(), value=string()}.
+
+%% @type rss_number_series() = [number()].
+
+-record(channel, {  %% required elements
+                    uri, title, desc, items, 
+                    %% optional elements
+                    language, copyright, managing_editor, web_master, 
+                    pub_date, last_build_date, category, generator, 
+                    docs, cloud, ttl, image, rating, text_input, 
+                    skip_hours, skip_days}).
+
+-record(rssitem, {  %% required elements
+                    uri, title, desc, 
+                    %% optional elements
+                    author, category,  comments,
+                    enclosure, guid, pub_date,  source}).
 
 %% @doc parse out an rss feed
 %% @spec process_rss(S::string()) -> [I]
@@ -50,9 +76,8 @@ parse_rss_item(I) when is_record(I, xmlElement) ->
   Link = xpath_text_content("//link", I), 
   Title = xpath_text_content("//title", I), 
   Desc = xpath_text_content("//description", I), 
-  Content = xpath_text_content("//content:encoded", I), 
   Date = xpath_text_content("//pubDate", I),
-  #rssitem{uri=Link, title=Title, desc=Desc, content=Content, dt=Date}.
+  #rssitem{uri=Link, title=Title, desc=Desc, pub_date=Date}.
 
 %% @private
 xpath_first_item(Str, R) when is_record(R, xmlElement) ->
